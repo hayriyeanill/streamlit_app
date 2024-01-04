1a74492707e5cabfe960e9a01872e6e1d61f7564
hayriyeanill <anill.hayriye@gmail.com>
2024-01-04 14:11:27 +0300

    Initial commit.

---
+++ my_penguins_charts.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+st.title("Palmer's Penguins")
+st.markdown("Use this Streamlit app to make your own scatterplot about penguins!")
+selected_species = st.selectbox("What species would you like to visualize?",
+                                ["Adelie", "Gentoo", "Chinstrap"])
+selected_x_var = st.selectbox('What do you want the x variable to be?',
+     ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
+selected_y_var = st.selectbox('What about the y?',
+     ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
+
+#import our dataset
+penguins_df = pd.read_csv('penguins.csv')
+penguins_df = penguins_df[penguins_df['species'] == selected_species]
+
+fig = px.scatter(penguins_df,
+                 x=selected_x_var,
+                 y=selected_y_var
+                 )
+
+fig = px.scatter(penguins_df,
+                 x=selected_x_var,
+                 y=selected_y_var
+                 )
+fig.update_layout({'title': f'Scatterplot of {selected_species} Penguins'} )
+st.plotly_chart(fig)
